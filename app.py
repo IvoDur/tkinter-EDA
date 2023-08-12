22bb04a7d2b3a14593d823ea94ad62b70f6a07b0
Ivo D'Ursi <ivomdursi@gmail.com>
2023-08-12 00:15:25 -0300

    Added basic plot

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+
 import pandas as pd
+
+import seaborn as sns
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 # Data
 data = pd.read_csv('placeholder.csv')
-print(list(data.iloc[1]))
 
 # Main window
 window = tk.Tk()
@@ -26,9 +30,19 @@ for index, row in data.iterrows():
 
 data_view_scrollbar.config(command=data_view.yview)
 
-# TODO: Scrollbar stick to data_view
-data_view.pack(side="left",fill="both")
-data_view_scrollbar.pack(side="right", fill="y")
+data_view.pack(side="left",fill="y")
+data_view_scrollbar.pack(side="left", fill="y")
+
+# Plot
+scatter_plot = Figure(figsize=(6, 6))
+ax = scatter_plot.subplots()
+sns.scatterplot(data=data.iloc[:,[1,3]], ax=ax)
+
+canvas = FigureCanvasTkAgg(figure=scatter_plot, master=window)
+canvas.draw()
+
+# TODO: Change pack to grid
+canvas.get_tk_widget().pack(side="right", before=data_view)
 
 # Input
 # Run
